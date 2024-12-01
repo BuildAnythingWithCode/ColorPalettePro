60acc5a9c9529080c45bdab8b5f792de87ebdf44
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-12-01 21:52:36 +0800

    Add basic functionality

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const button = document.querySelector("button");
+const colorSection = document.querySelector("#color-section");
 
-button.addEventListener('click', function() {
-  
-})
+randomizeColorFunction = function() {
+  let r = Math.floor(Math.random()* 256);
+  let g = Math.floor(Math.random()* 256);
+  let b = Math.floor(Math.random()* 256);
+  let randomColor = `rgb(${r}, ${g}, ${b})`;
+  colorSection.style.backgroundColor = randomColor;
+}
+
+button.addEventListener('click', randomizeColorFunction);

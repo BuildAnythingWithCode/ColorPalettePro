a8e262d51c878310e9300ff3b6b8bbcd60362f8d
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-12-01 23:03:37 +0800

    Add random color functionality to new colors

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const generateRandomColorButton = document.querySelector("#generate-random-color");
 const addColorButton = document.querySelector("#add-color");
-const color = document.querySelector(".color");
+
 const colorSection = document.querySelector("#color-section");
 
 randomizeColorFunction = function() {
+  const color = document.querySelectorAll(".color");
+  
+  for (let i = 0; i < color.length; i++) {
   let r = Math.floor(Math.random()* 256);
   let g = Math.floor(Math.random()* 256);
   let b = Math.floor(Math.random()* 256);
   let randomColor = `rgb(${r}, ${g}, ${b})`;
-  color.style.backgroundColor = randomColor;
+  color[i].style.backgroundColor = randomColor;
+  }
 }
 
 addColorFunction = function() {
